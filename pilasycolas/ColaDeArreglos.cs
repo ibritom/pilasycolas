8f053e243b07df49466af7ae8d046b39d2534ab7
PICADO PICADO JEISON JOHEL <jeijoel@estudiantec.cr>
2025-03-21 20:05:16 -0600

    Agregué las clases ColaDeArreglos y ColaDeListas

---
+++ pilasycolas/ColaDeArreglos.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilasycolas
+{
+    public class ColaDeArreglos : Cola
+    {
+        private int[] cola;
+        private int delante;
+        private int destras;
+        private int tamano;
+
+        public ColaDeArreglos(int tamano)
+        {
+            this.tamano = tamano;
+            cola = new int[tamano];
+            delante = -1;
+            destras = -1;
+        }
+
+        public void AnadirACola(int elemento)
+        {
+            if (destras == tamano - 1)
+            {
+                Console.WriteLine("Desbordamiento de la cola");
+            }
+            else
+            {
+                if (delante == -1)
+                {
+                    delante = 0;
+                }
+                cola[++destras] = elemento;
+            }
+        }
+
+        public int QuitarDeCola()
+        {
+            if (delante == -1)
+            {
+                Console.WriteLine("Desfase de cola");
+                return -1;
+            }
+            else
+            {
+                int elemento = cola[delante];
+                if (delante == destras)
+                {
+                    delante = -1;
+                    destras = -1;
+                }
+                else
+                {
+                    delante++;
+                }
+                return elemento;
+            }
+        }
+
+        public int Delante()
+        {
+            if (delante == -1)
+            {
+                Console.WriteLine("Lista desfasada");
+                return -1;
+            }
+            else
+            {
+                return cola[delante];
+            }
+        }
+    }
+
+}
