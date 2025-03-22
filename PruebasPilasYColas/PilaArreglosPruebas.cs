c4422b58aeb2cdc52f53cc247298142ae30e146b
PICADO PICADO JEISON JOHEL <jeijoel@estudiantec.cr>
2025-03-22 12:34:47 -0600

    Implementa las clase de pruebas PilaArreglosPruebas y PilaListasPruebas

--- PruebasPilasYColas/PilaArreglosPruebas.cs
+++ PruebasPilasYColas/PilaArreglosPruebas.cs
@@ -5,8 +5,51 @@ namespace PruebasPilasYColas
     public sealed class PilaArreglosPruebas
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PushPruebas()
         {
+            PilaArreglos pila = new PilaArreglos(5);
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+            pila.Push(4);
+            pila.Push(5);
+            Assert.AreEqual("1, 2, 3, 4, 5", pila.ToString());
+        }
+
+        [TestMethod]
+        public void PopPruebas()
+        {
+            PilaArreglos pila = new PilaArreglos(5);
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+            pila.Push(4);
+            pila.Push(5);
+            pila.Pop();
+            Assert.AreEqual("1, 2, 3, 4", pila.ToString());
+        }
+
+        [TestMethod]
+        public void TopPruebas()
+        {
+            PilaArreglos pila = new PilaArreglos(5);
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+            pila.Push(4);
+            pila.Push(5);
+            Assert.AreEqual(5, pila.Top());
+        }
+        [TestMethod]
+        public void ToStringPruebas()
+        {
+            PilaArreglos pila = new PilaArreglos(5);
+            pila.Push(1);
+            pila.Push(2);
+            pila.Push(3);
+            pila.Push(4);
+            pila.Push(5);
+            Assert.AreEqual("1, 2, 3, 4, 5", pila.ToString());
         }
     }
 }
--- PruebasPilasYColas/PilaListasPruebas.cs
+++ PruebasPilasYColas/PilaListasPruebas.cs
@@ -5,7 +5,48 @@ namespace PruebasPilasYColas;
 public class PilaListasPruebas
 {
     [TestMethod]
-    public void TestMethod1()
+    public void PushPruebas()
     {
+        PilaListas pila = new PilaListas();
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+        pila.Push(4);
+        pila.Push(5);
+        Assert.AreEqual("5, 4, 3, 2, 1", pila.ToString());
+    }
+    [TestMethod]
+    public void PopPruebas()
+    {
+        PilaListas pila = new PilaListas();
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+        pila.Push(4);
+        pila.Push(5);
+        pila.Pop();
+        Assert.AreEqual("4, 3, 2, 1", pila.ToString());
+    }
+    [TestMethod]
+    public void TopPruebas()
+    {
+        PilaListas pila = new PilaListas();
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+        pila.Push(4);
+        pila.Push(5);
+        Assert.AreEqual(5, pila.Top());
+    }
+    [TestMethod]
+    public void ToStringPruebas()
+    {
+        PilaListas pila = new PilaListas();
+        pila.Push(1);
+        pila.Push(2);
+        pila.Push(3);
+        pila.Push(4);
+        pila.Push(5);
+        Assert.AreEqual("5, 4, 3, 2, 1", pila.ToString());
     }
 }
